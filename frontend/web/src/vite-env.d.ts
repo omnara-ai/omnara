67c6dd2335427f2721a226efc5dba2bfaa1da52b
ishaansehgal99 <ishaanforthewin@gmail.com>
2025-09-16 15:33:46 -0500

    fix: remove nits

--- frontend/web/src/vite-env.d.ts
+++ frontend/web/src/vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_POSTHOG_KEY?: string
-  readonly VITE_POSTHOG_HOST?: string
-  readonly VITE_SENTRY_DSN?: string
-  readonly SENTRY_DSN?: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
